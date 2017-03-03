3b2d26b0024a321562e54e0e36fbabc7154bfb2b
Yassine Elabbassi <yassine@elabbassi.org>
2017-03-03 11:41:52 -0300

    question view and controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,45 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+
+    if @flat.save
+      # redirect_to questions_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      #redirect to question all
+    else
+      render :edit
+    end
   end
+
+
+
+private
+
+
+def question_params
+  params.require(:question).permit(:content)
 end
+
+
+
+end
+
