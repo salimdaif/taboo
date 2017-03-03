5ae9baf64f4f03ccd06e091993f5e976fcc3e7c1
Yassine Elabbassi <yassine@elabbassi.org>
2017-03-03 12:24:00 -0300

    question all work

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
-    if @flat.save
+    if @question.save
       redirect_to questions_path
     else
       render :new
