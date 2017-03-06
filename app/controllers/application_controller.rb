061b701f3f5989ac36a995833c4eceadf3b82e0e
Salim Daif <salim.daif@gmail.com>
2017-03-06 11:30:47 -0300

    Modal done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :redirect_to_questions, unless: -> { current_user.answered_minimum_questions? }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age])
+  end
+
+  def redirect_to_questions
+    redirect_to user_path(current_user)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  skip_before_action :redirect_to_questions, only: :home
 
   def home
   end
