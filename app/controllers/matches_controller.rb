2d353cca3957189b84c60e5a7e5c582a065ba283
Yassine Elabbassi <yassine@elabbassi.org>
2017-03-09 15:39:47 -0300

    cards generated correctly

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,7 @@
 class MatchesController < ApplicationController
   def index
-    # @scores_hash = current_user.add_scores
+    @matches_hash = current_user.add_scores
+    @matches_hash = @matches_hash.sort_by { |id, score| score }.reverse
     @user = User.all
   end
 end
