4836e063fffee1eea9c27c57df6863b62b255af5
Salim Daif <salim.daif@gmail.com>
2017-03-10 17:40:57 -0300

    modal front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  skip_before_action :redirect_to_questions, only: :home
+  skip_before_action :redirect_to_custom_path
 
   def home
+  end
+
+  def intro
   end
 end
