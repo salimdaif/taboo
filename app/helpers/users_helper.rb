0dc7e8b8c7acf81dcdecb33bd5b67fe687d415e6
Yassine Elabbassi <yassine@elabbassi.org>
2017-03-15 13:12:26 -0300

    fixed tab bug

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+  def tab_hidden(tab, params)
+    if tab == 'questionnaire' && !params.present?
+      ""
+    elsif tab != params
+      "hidden"
+    end
+  end
+
+
+  def tab_active(tab, params)
+    if tab == params
+      "active"
+    elsif tab == 'questionnaire' && !params.present?
+        "active"
+    end
+
+  end
 end
