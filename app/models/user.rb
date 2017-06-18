9904a5bfca3355600ca3839df638efa5892ac9fa
salimdaif <salim.daif@gmail.com>
2017-06-18 11:52:06 -0400

    Online/Offline bar off

--- app/models/user.rb
+++ app/models/user.rb
@@ -176,12 +176,12 @@ class User < ApplicationRecord
      (scores.inject(&:+) / scores.length.to_f)
   end
 
-  def online?
-    if ((DateTime.current - self.last_sign_in_at.to_datetime).to_i * 24) < 5
-      true
-    else
-      false
-    end
-  end
+#  def online?
+ #   if ((DateTime.current - self.last_sign_in_at.to_datetime).to_i * 24) < 5
+  #    true
+    #else
+   #   false
+    #end
+  #end
 
 end
